7d7b75a5c979b7913a3289bd8af52365c8d7fdf8
GameRock <kokhanchuk2022work@gmail.com>
2026-03-18 18:04:35 +0100

    plugin displays the 3d model, but drop does not work properly

--- src/chrome-plugin/simple_script.js
+++ src/chrome-plugin/simple_script.js
@@ -1,3 +1,10 @@
 document.getElementById("toggleColor").addEventListener("click", () => {
     chrome.runtime.sendMessage({ action: "toggleBackgroundColor" });
 });
+
+
+document.getElementById("openViewer").addEventListener("click", () => {
+    chrome.tabs.create({
+        url: "index.html"
+    });
+});
